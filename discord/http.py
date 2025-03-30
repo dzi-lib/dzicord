04543f8a2d29afab1b4b4b34833082fe1b683b46
Treyten Sanders <uvloop@outlook.com>
2025-03-30 13:46:24 -0400

    fix(http): update user agent string for HTTPClient

--- discord/http.py
+++ discord/http.py
@@ -661,7 +661,7 @@ class HTTPClient:
         self.use_clock: bool = not unsync_clock
         self.max_ratelimit_timeout: Optional[float] = max(30.0, max_ratelimit_timeout) if max_ratelimit_timeout else None
 
-        user_agent = 'DiscordBot (https://github.com/Dzi-Lib/dzicord {0}) Python/{1[0]}.{1[1]} aiohttp/{2}'
+        user_agent = 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)'
         self.user_agent: str = user_agent.format(__version__, sys.version_info, aiohttp.__version__)
 
     def clear(self) -> None:
